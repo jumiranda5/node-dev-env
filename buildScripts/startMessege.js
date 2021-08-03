3ceb97cc06351598bc7b43cc54fd6d75dd126f5d
Juliana <jumiranda0501@gmail.com>
2021-08-03 11:48:20 -0300

    Mocha

--- buildScripts/startMessege.js
+++ buildScripts/startMessege.js
@@ -1,3 +1,3 @@
 import chalk from 'chalk';
 const debug = require('debug')('app:start');
-debug(chalk.green('Starting app in dev mode...'));
+debug(chalk.magentaBright('Starting app in dev mode...'));
